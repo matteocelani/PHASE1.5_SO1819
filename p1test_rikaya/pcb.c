15399b2a0362e1d29d5e55b609361928000341f0
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-02-14 21:42:42 +0100

    Update pcb.c

--- p1test_rikaya/pcb.c
+++ p1test_rikaya/pcb.c
@@ -3,10 +3,62 @@
 #include <typer_rikaya.H>
 #include <listx.h>
 
+HIDDEN pcb_t pcbFree_table[MAXPROC];
+
+/* Lista con sentinella dei pcb liberi o inutilizzati */
+LIST_HEAD(pcbFree_h);
+
+
 /* PCB free list handling functions */
-void initPcbs(void);
-void freePcb(pcb_t *p);
-pcb_t *allocPcb(void);
+
+/* Inizializza la pcbFree in modo da contenere tutti gli elementi della pcbFree_table. */
+/* Questo metodo deve essere chiamato una volta sola in fase di inizializzazione della struttura dati. */
+void initPcbs(void){
+    int i=0;
+    for (i;i<MAXPROC;i++){
+        /* Metto nella lista pcbFree_h i puntatori ai pcb */
+        pcb_t* pcb= &pcbFree_table[MAXPROC];
+        list_add_tail(&(pcb.p_next),&(pcbFree_h))
+        ;
+
+    }
+}
+
+/* Inserisce il pcb puntato da p nella lista dei PCB liberi */
+void freePcb(pcb_t *p){
+    list_add_tail(&(p->p_next, &(pcbFree_h);
+}
+
+
+/* Restituisce NULL se la pcbFree +vuota.                                 */
+/* Altrimenti rimuove un elemento dalla pcbFree,                          */
+/* inizializza tutti i campi (NULL\0) e restituisce l'elemento rimosso    */
+pcb_t *allocPcb(void){
+    /* Controllo per verificare se pcbFree sia vuoto o no */
+    if (list_empty(&(pcbFree_h))) return NULL;
+
+    /* Elemento precedente alla sentinella è l'ultimo elemento della lista */
+    /* */
+    pcb_t *tmp=container_of(pcbFree_h.prev, pcb_t, p_next);
+    
+    /* Rimuove tmp dalla lista pcbFree_H che lo contiene */
+    list_del(tmp->p_next);
+
+    /* Inizializzo tutti i campi di tmp a NULL */
+    tmp->p_parent=NULL;
+    tmp->p_semkey=NULL;
+    tmp->priority=0;
+    
+    /* tmp->p_s bisogna azzerare tutta la struttura */
+    /* non so quali siano i campi, vanno cercati nel manuale */
+
+
+    INIT_LIST_HEAD(tmp->p_next);
+    INIT_LIST_HEAD(tmp->p_child);
+    INIT_LIST_HEAD(tmp->p_sib);
+
+    return tmp;
+}
 
 /* PCB queue handling functions */
 void mkEmptyProcQ(struct list_head *head);
