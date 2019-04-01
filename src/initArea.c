afd14fd60d5ad9f177333ea5dd9abebcce4572c7
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-02 00:47:43 +0200

    Update initArea.c

--- src/initArea.c
+++ src/initArea.c
@@ -9,33 +9,33 @@
 #include "p1.5test_rikaya_v0.c"
 
 /* Funzioni per l'inizializzazione delle NEW AREA */
-HIDDEN inline void initSYS(u32 status){
-    sys_newarea->status = status;
-    state->reg_sp = RAMTOP;
+HIDDEN inline void initSYSBK(u32 status){
+    sysbk_newarea->status = status;
+    sysbk_newarea->reg_sp = RAMTOP;
     /* Indirizzo della funzione che gestisce le SYSCALL */
-    state->pc_epc = sys_handler;  // La funzione la dobbiamo scrivere, intanto metto il nome
-    state->reg_t9 = sys_handler;
+    sysbk_newarea->pc_epc = sys_handler;  // La funzione la dobbiamo scrivere, intanto metto il nome
+    sysbk_newarea->reg_t9 = sys_handler;
 }
 
 HIDDEN inline void initPGMTRP(u32 status){  
     program_trap_newarea->status = status;
-    state->reg_sp = RAMTOP;
-    state->pc_epc = gmtrp_handler;
-    state->reg_t9 = gmtrp_handler;
+    program_trap_newarea->reg_sp = RAMTOP;
+    program_trap_newarea->pc_epc = gmtrp_handler;
+    program_trap_newarea->reg_t9 = gmtrp_handler;
 }
     
 HIDDEN inline void initINT(u32 status){  
     interrupt_newarea->status = status;
-    state->reg_sp = RAMTOP;
-    state->pc_epc = int_handler;
-    state->reg_t9 = int_handler;
+    interrupt_newarea->reg_sp = RAMTOP;
+    interrupt_newarea->pc_epc = int_handler;
+    interrupt_newarea->reg_t9 = int_handler;
 }
 
 HIDDEN inline void initTLB(u32 status){  
     tblmgt_newarea->status = status;
-    state->reg_sp = RAMTOP;
-    state->pc_epc = tlb_handler;
-    state->reg_t9 = tlb_handler;
+    tblmgt_newarea->reg_sp = RAMTOP;
+    tblmgt_newarea->pc_epc = tlb_handler;
+    tblmgt_newarea->reg_t9 = tlb_handler;
 }
 
 /* Funzione che si occupa dell'inizializzazione delle NEWAREA della ROM.
