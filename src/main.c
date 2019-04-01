d76e3423bbeefcbede3e3ca95ab56f40def0bb05
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-01 21:42:12 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,10 +7,49 @@
 #include "types_rikaya.h"
 #include "p1.5test_rikaya_v0.c"
 
+/* Lista dei processi ready */
 INIT_LIST(ready_queue);
-pcb_t current_process;
-u32 process_count;
-u32 soft_block_count;
+/* Puntatore al processo attivo */
+pcb_t current_process = NULL;
+/* Contatore processi */
+u32 process_count = 0;
+/* Contatore processi bloccati per I/O */
+u32 soft_block_count = 0;
+
+/* Puntatori alle NEW AREA della ROM */
+HIDDEN state_t *sys_newarea;
+HIDDEN state_t *program_trap_newarea;
+HIDDEN state_t *interrupt_newarea;
+HIDDEN state_t *tblmgt_newarea;
+
+/* Funzioni per l'inizializzazione delle NEW AREA */
+inline void initSYS(u32 status){
+    sys_newarea->status = status;
+    state->reg_sp = RAMTOP;
+    /* Indirizzo della funzione che gestiste le SYSCALL */
+    state->pc_epc = sys_handler;  // La funzione la dobbiamo scrivere, intanto metto il nome
+    state->reg_t9 = sys_handler;
+}
+// Bisogna fare la stessa cosa fatta per sys_newarea per gli altri state
+// gli indirizzi sono scritti in const.h
+inline void initPGMTRP(u32 status){    
+}
+    
+inline void initINT(u32 status){   
+}
+
+inline void initTLB(u32 status){   
+}
+
+void initNEWAREA(void){
+    u32 s = 0;
+    // intanto metto zero, ma andrà cambiato
+    initSYS(s);
+    initPGMTRAP(s);
+    initINT(s);
+    initTLB(s);
+}
+
 
 int main(void){
     
