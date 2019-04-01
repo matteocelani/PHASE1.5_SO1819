ecfd5e8662c61d2f551416fe99c83ecc822d3169
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-01 14:37:01 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,10 +8,14 @@
 #include "p1.5test_rikaya_v0.c"
 
 INIT_LIST(ready_queue);
+pcb_t current_process;
+u32 process_count;
+u32 soft_block_count;
 
 int main(void){
     
-    initPCB();
+    initPcbs();
+    
 
 
 
