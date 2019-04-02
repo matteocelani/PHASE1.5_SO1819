b2736dd924dd192bbf3085cf98d5438f1e0e3cd4
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-02 23:44:34 +0200

    Update handler.c

--- src/handler.c
+++ src/handler.c
@@ -1,3 +1,6 @@
 void sysbkhandler(void){
-    state_t old;
+    /* Stato dell'esecuzione prima dell'eccezione */
+    state_t *old_state = sysbk_oldarea;
+    /* Registro nel quale è salvata la SYSCALL chiamata */
+    u32 syscall_number = old_state->reg_a0   
 }
