1c43c8e58220fff11ff9abbf70ed205dec3c75ec
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2019-04-03 10:21:55 +0200

    Update handler.c

--- src/handler.c
+++ src/handler.c
@@ -1,3 +1,10 @@
+#include "pcb.h"
+#include "const.h"
+#include "handler.h"
+#include "listx.h"
+#include "types_rikaya.h"
+#include "p1.5test_rikaya_v0.c"
+
 void sysbk_handler(void){
     /* Stato dell'esecuzione prima dell'eccezione */
     state_t *old_state = sysbk_oldarea;
